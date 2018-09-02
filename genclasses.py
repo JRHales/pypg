70f25a5a5a8d03c6376a0289ed48a4483bb09158
Joseph R. Hales <Joseph.hales1@gmail.com>
2018-09-02 12:16:04 -0400

    Update genclasses.py

--- genclasses.py
+++ genclasses.py
@@ -1,6 +1,16 @@
 class Player:
-  def __init__(self, name, strength, defence):
+  def __init__(self, name, strength):
     self.name = name
     self.strength = strength
-    self.defence = defence
-    self.hitpoints = (strength+defence)*3
+    self.weaponDamage = 0
+    self.attack = 0
+    self.armor = 0
+    self.defence = 0
+    self.hitpoints = (strength+(strength/2))*3
+  
+  def setAttack(self):
+    self.attack = self.strength+self.weaponDamage
+
+  def setWeaponDamage(self, weapon):
+    self.weaponDamage = weapon 
+    self.setAttack()
