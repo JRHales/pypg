d6808301646223548911e1f0794198a600398521
Joseph R. Hales <Joseph.hales1@gmail.com>
2018-09-27 11:09:42 -0400

    Update genclasses.py

--- genclasses.py
+++ genclasses.py
@@ -1,17 +1,17 @@
 class Player:
   def __init__(self, name):
     self.name = name
-    self.strength = 5
-    self.vitality = 5
-    self.energy = 5
+    self.strength = 1
+    self.vitality = 1
+    self.energy = 1
     self.luck = 1
-    self.agility = 5
-    self.power = 5
+    self.agility = 1
+    self.power = 1
     
     self.weaponDamage = 0
-    self.attack = 0
+    self.attack = 1
     self.armor = 0
-    self.defence = 0
+    self.defence = 1
     self.hitpoints = round((self.vitality+(self.strength/2)+(self.energy/3)))
   
   def setAttack(self):
@@ -23,3 +23,10 @@ class Player:
     
   def maxHp(self):
     self.hitpoints = round((self.vitality+(self.strength/2)+(self.energy/3)))
+   
+  def setDefence(self):
+    self.defence = self.strength+self.armor
+  
+  def setArmorDefence(self, armor):
+    self.armor = armor
+    self.setDefence()
