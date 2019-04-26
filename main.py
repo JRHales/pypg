0d68f505db66bc3957a178223cf09b85cf177fe8
Joseph R. Hales <Joseph.hales1@gmail.com>
2019-04-26 12:59:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from tkinter import *
 import genclasses
 
 def main():
+  WINDOWSIZE = '600x400'
+  window = TK()
+  window.title("pypg")
+  window.geometry(WINDOWSIZE)
   print("Begun") #Does it even start
+  lbl = Label(window, text="hello", font=("Arial Bold", 50))
+  lbl.grid(column=0, row=0)
   player1 = genclasses.Player("Test Player") #Player name is set to test Player. Make actual name input.
-  print(player1.name) #this may be private and need a get function. 
+  print(player1.getName()) 
+  
+  window.mainloop()
+  
 
 main()
